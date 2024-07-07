13414c77e78d7dc80c9bf716c67891fb2e9a6a08
aa827 <hjkim27.dev@gmail.com>
2024-07-07 22:24:08 +0900

    [ADD] 기본 util 및 설정 추가
    - interceptor
    - jquery
    - login, cookie util

---
+++ src/main/java/com/example/demo/config/ProjectInterceptor.java
@@ -0,0 +1,26 @@
+package com.example.demo.config;
+
+import com.example.demo.util.LoginUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author hjkim27
+ * @date 2024. 07. 07
+ */
+@Slf4j
+public class ProjectInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (LoginUtil.isLogin(request, response)) {
+            return true;
+        } else {
+            response.sendRedirect(request.getContextPath() + "/login");
+            return false;
+        }
+    }
+}
---
+++ src/main/java/com/example/demo/util/LoginUtil.java
@@ -0,0 +1,104 @@
+package com.example.demo.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * <pre>
+ *     로그인, 로그아웃, 로그인 확인을 위한 유틸리티 클래스
+ * </pre>
+ *
+ * @author hjkim27
+ * @date 2024 .07. 07
+ */
+@Slf4j
+public class LoginUtil {
+
+    // sid가 저장되는 cookie 이름
+    public static final String COOKIE_NAME = "hjkim27LoginId";
+
+    /**
+     * <pre>
+     *     사용자의 권한 확인
+     * </pre>
+     *
+     * @param request
+     * @return
+     */
+    public static int getAdminId(HttpServletRequest request) {
+        int id = -1;
+        try {
+            String adminSidStr = CookieUtil.getCookie(request, COOKIE_NAME);
+            if (adminSidStr != null) {
+                id = Integer.parseInt(adminSidStr);
+            }
+        } catch (NumberFormatException nfe) {
+            log.error("로그인 정보를 가져 오는 도중에 오류가 발생하였습니다.");
+        } catch (Exception e) {
+            log.error("로그인 정보를 가져 오는 도중에 오류가 발생하였습니다.\n{} | {}", e, e.getMessage());
+        }
+
+        return id;
+    }
+
+
+    /**
+     * <pre>
+     *     사용자 로그인여부 확인
+     * </pre>
+     *
+     * @param request
+     * @param response
+     * @return
+     */
+    public static boolean isLogin(HttpServletRequest request, HttpServletResponse response) {
+        boolean isLogin = false;
+        try {
+            String adminSidStr = CookieUtil.getCookie(request, COOKIE_NAME);
+            if (adminSidStr != null) {
+                isLogin = true;
+            }
+        } catch (Exception e) {
+            log.error("{} | {}", e, e.getMessage());
+            log.error("로그인 정보를 확인 하는 도중에 오류가 발생하였습니다.");
+        }
+        return isLogin;
+    }
+
+    /**
+     * <pre>
+     *     로그인: cookie 로 저장
+     * </pre>
+     *
+     * @param request
+     * @param response
+     * @param sid      사용자 sid
+     */
+    public static void setLogin(HttpServletRequest request, HttpServletResponse response, int sid) {
+        try {
+            CookieUtil.setCookie(response, COOKIE_NAME, Integer.toString(sid));
+        } catch (Exception e) {
+            log.error("로그인 정보를 입력 하는 도중 오류가 발생하였습니다.\n{} | {}", e, e.getMessage());
+        }
+    }
+
+    /**
+     * <pre>
+     *     로그아웃
+     * </pre>
+     *
+     * @param request
+     * @param response
+     */
+    public static void setLogout(HttpServletRequest request, HttpServletResponse response) {
+        int sid = getAdminId(request);
+        if (sid == -1) {
+            return;
+        }
+        CookieUtil.removeCookie(response, COOKIE_NAME);
+
+    }
+
+}
