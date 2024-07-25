b2f806e7ad2f39cb85d728873f5cc44c01e8db93
aa827 <hjkim27.dev@gmail.com>
2024-07-26 00:04:45 +0900

    [FIX] database 수정중 임시 commit

--- sql/clean_database.sql
+++ sql/clean_database.sql
@@ -35,38 +35,72 @@ create table tb_setting_info
 create table tb_project_repository
 (
     sid         serial,
-    repo_name   character varying(100) not null,
-    description character varying,
-    repo_type   character varying(10) default 'public',
-    admin_sid   integer                not null
+    name        character varying(100) not null,               -- commit.owner.name
+    full_name   character varying(100) not null,               -- commit.owner.fullName
+    description character varying,                             -- commit.owner.description
+    private     boolean                         default false, -- commit.owner.private
+    html_url    character varying,                             -- commit.owner.htmlUrl
+    ssh_url     character varying,                             -- commit.owner.sshUrl
+    active      boolean                not null default true
 );
 
--- branch
-create table tb_repository_branch
+-- owner
+create table tb_project_owner
 (
-    sid            serial,
-    branch_name    character varying(100) not null,
-    description    character varying,
-    repository_sid int                    not null
+    sid      serial,
+    login    character varying(100) not null, -- commit.owner.owner.login
+    name     character varying(100) not null, -- commit.owner.owner.name
+    html_url character varying(100) not null  -- commit.owner.owner.htmlUrl
+);
+
+--  commit 내역
+create table tb_project_commit
+(
+    sid             serial,
+    sha             character varying(100) unique not null, -- commit.tree.sha
+    parent_sha      character varying(100) unique not null, -- commit.parentSHA1s(0)
+    committer_name  character varying(50)         not null, -- commit.commitShortInfo.committer.name
+    committer_email character varying(50)         not null, -- commit.commitShortInfo.committer.email
+    committer_date  timestamp with time zone      not null, -- commit.commitShortInfo.committer.date
+    message         text                          not null, -- commit.commitShortInfo.message
+    html_url        character varying(255) unique not null, -- commit.htmlUrl
+    repository_sid  integer                       not null  -- tb_project_repository.sid
+);
+
+-- labels
+create table tb_project_label
+(
+    sid         serial,
+    label_id    character varying(15) unique not null, -- commit.owner.listLabels(n).id
+    name        character varying(15) unique not null, -- commit.owner.listLabels(n).name
+    description character varying(15) unique not null, -- commit.owner.listLabels(n).description
+    color       character varying(15) unique not null, -- commit.owner.listLabels(n).color
+    active      boolean                      not null default true
 );
 
 -- issue
-create table tb_repository_issue
+-- commit.getOwner().getIssue(GHIssueState.ALL) > for
+create table tb_project_issue
 (
     sid            serial,
-    labels         character varying,
-    subject        character varying,
-    content        character varying(10000),
-    repository_sid int not null
+    state          character varying(6) not null default 'OPEN', -- issue.state
+    number         integer              not null,                -- issue.number
+    title          character varying    not null,                -- issue.title
+    body           character varying    not null,                -- issue.body
+    repository_sid integer              not null,                -- tb_project_repository.sid
+    label_sids     character varying    not null,                -- tb_project_label.sid.join(',')
+    active         boolean              not null default true
 );
 
--- label
-create table tb_repository_label
+-- comment
+-- commit.getOwner().getComments() > for
+create table tb_project_comment
 (
-    sid            serial,
-    label_name     character varying(20) unique not null,
-    description    character varying,
-    label_color    character varying(20)        not null,
-    label_bg_color character varying(20)        not null
+    sid               serial,
+    comment_id        character varying not null, -- comment.id
+    body              text              not null, -- comment.body
+    parent_comment_id character varying,          -- comment.parent.id
+    created_at        timestamp with time zone,   -- comment.createdAt
+    updated_at        timestamp with time zone,   -- comment.updatedAt
+    active            boolean           not null default true
 );
-
